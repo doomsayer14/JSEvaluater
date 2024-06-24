cee5fc83b97e3beb1cdd51274ff68251bc61dbc3
vyesman <viktor.yesman@ukr.net>
2024-06-24 23:13:28 +0300

    test commit x2

--- src/main/java/com/example/jsevaluater/JsEvaluaterApplication.java
+++ src/main/java/com/example/jsevaluater/JsEvaluaterApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JsEvaluaterApplication {
-//test
     public static void main(String[] args) {
         SpringApplication.run(JsEvaluaterApplication.class, args);
     }
-
 }
